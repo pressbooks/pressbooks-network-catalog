528ac620d41236c7a303ec467ddcecb2e6c3c88b
arzola <oscararzola@gmail.com>
2022-08-24 15:53:49 -0400

    feat: title and description

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,5 +1,10 @@
 @php( get_header() )
-
+<div class="flex flex-col text-center px-2">
+    <h3>{{ get_the_title() }}</h3>
+    <div>
+        {!! wp_kses_post(get_the_content()) !!}
+    </div>
+</div>
 <main class="app">
 	<aside id="filters" style="padding: 2rem 0; min-width: 300px;">
 		@include('PressbooksNetworkCatalog::partials.filters')
