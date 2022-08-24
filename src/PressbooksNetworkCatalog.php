9435e52f37cbd1b4ae1fc9d50be6685c0d322b68
arzola <oscararzola@gmail.com>
2022-08-24 09:26:02 -0400

    fix: enqueue scripts

--- src/PressbooksNetworkCatalog.php
+++ src/PressbooksNetworkCatalog.php
@@ -6,80 +6,84 @@ use Pressbooks\Container;
 
 class PressbooksNetworkCatalog
 {
-	protected static ?PressbooksNetworkCatalog $instance = null;
+    protected static ?PressbooksNetworkCatalog $instance = null;
 
-	public static function init(): self
-	{
-		if (! static::$instance) {
-			static::$instance = new static;
+    public static function init(): self
+    {
+        if (! static::$instance) {
+            static::$instance = new static;
 
-			static::$instance->setUp();
-		}
+            static::$instance->setUp();
+        }
 
-		return static::$instance;
-	}
+        return static::$instance;
+    }
 
-	public function setUp(): void
-	{
-		$this->setUpViteConfig();
+    public function setUp(): void
+    {
+        $this->enqueueScripts();
 
-		$this->setUpBlade();
+        $this->setUpBlade();
 
-		$this->addHooks();
-	}
+        $this->addHooks();
+    }
 
-	protected function setUpViteConfig(): void
-	{
-		/**
-		 * VITE & Tailwind JIT development
-		 * Inspired by https://github.com/andrefelipe/vite-php-setup
-		 */
-		add_action('wp_enqueue_scripts', function () {
-			if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT) {
-				// insert hmr into head for live reload
-				add_action('wp_head', function () {
-					echo '<script type="module" crossorigin src="http://127.0.0.1:3000/index.js"></script>';
-				});
+    protected function enqueueScripts(): void
+    {
+        add_action(
+        /**
+         * @throws \JsonException
+         */ 'wp_enqueue_scripts', function () {
 
-				return;
-			}
+            /**
+             * VITE & Tailwind JIT development
+             * Inspired by https://github.com/andrefelipe/vite-php-setup
+             */
+            if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT) {
+                // insert hmr into head for live reload
+                add_action('wp_head', function () {
+                    echo '<script type="module" crossorigin src="http://127.0.0.1:3000/index.js"></script>';
+                });
 
-			$distUri = plugin_dir_url(__DIR__).'/dist';
-			$distPath = plugin_dir_path(__DIR__).'/dist';
+                return;
+            }
 
-			// production version, 'npm run build' must be executed in order to generate assets
-			$manifest = json_decode(file_get_contents("$distPath/manifest.json"), true);
+            $distUri = plugin_dir_url(__DIR__).'/dist';
+            $distPath = plugin_dir_path(__DIR__).'/dist';
 
-			if (is_array($manifest)) {
-				$manifest_key = array_keys($manifest);
+            // production version, 'npm run build' must be executed in order to generate assets
+            $manifest = json_decode(file_get_contents("$distPath/manifest.json"), true, 512, JSON_THROW_ON_ERROR);
 
-				if (isset($manifest_key[0])) {
-					// enqueue CSS files
-					foreach ($manifest[$manifest_key[0]]['css'] ?? [] as $css_file) {
-						wp_enqueue_style('main', "$distUri/$css_file");
-					}
+            if (is_array($manifest)) {
+                $manifest_key = array_keys($manifest);
 
-					// enqueue main JS file
-					$js_file = $manifest[$manifest_key[0]]['file'] ?? null;
-					if (! empty($js_file)) {
-						wp_enqueue_script('main', "$distUri/$js_file", [], '', true);
-					}
-				}
-			}
-		});
-	}
+                if (isset($manifest_key[0])) {
+                    // enqueue CSS files
+                    foreach ($manifest[$manifest_key[0]]['css'] ?? [] as $css_file) {
+                        wp_enqueue_style('pb-network-catalog/style', "$distUri/$css_file", ['aldine/style']); // override Aldine's
+                    }
 
-	protected function setUpBlade(): void
-	{
-		Container::get('Blade')
-			->addNamespace(
-				'PressbooksNetworkCatalog',
-				dirname(__DIR__).'/resources/views'
-			);
-	}
+                    // enqueue main JS file
+                    $js_file = $manifest[$manifest_key[0]]['file'] ?? null;
+                    if (! empty($js_file)) {
+                        wp_enqueue_script('pb-network-catalog/script', "$distUri/$js_file", [], '', true);
+                    }
+                }
+            }
+        });
+    }
 
-	protected function addHooks(): void
-	{
-		add_filter('pb_network_catalog', fn () => (new CatalogManager)->handle());
-	}
+    protected function setUpBlade(): void
+    {
+        Container::get('Blade')
+            ->addNamespace(
+                'PressbooksNetworkCatalog',
+                dirname(__DIR__).'/resources/views'
+            );
+    }
+
+    protected function addHooks(): void
+    {
+        add_filter('pb_network_catalog', fn() => (new CatalogManager)->handle());
+    }
 }
