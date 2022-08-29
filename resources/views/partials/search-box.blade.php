cf45cb34b4ad29b4027fdf9c66994ae801c660f4
arzola <oscararzola@gmail.com>
2022-08-29 15:57:56 -0400

    feat: parse filters query string

--- resources/views/partials/search-box.blade.php
+++ resources/views/partials/search-box.blade.php
@@ -1,24 +1,26 @@
-<div class="search-box">
-	<div class="search">
-		<label id="search-input-label" class="sr-only">{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}</label>
-		<input type="search" placeholder="{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}" aria-labelledby="search-input-label"/>
-		<button>{{ __('Search', 'pressbooks-network-catalog') }}</button>
-	</div>
+<form action="/catalog" method="get">
+    <div class="search-box">
+        <div class="search">
+            <label id="search-input-label" class="sr-only">{{ __('Search', 'pressbooks-network-catalog') }}</label>
+            <input x-data type="search" name="search" placeholder="{{ __('Find a book', 'pressbooks-network-catalog') }}" aria-labelledby="search-input-label" x-model="$store.filters.search"/>
+            <button type="submit">{{ __('Search', 'pressbooks-network-catalog') }}</button>
+        </div>
 
-	<label id="per-page-label" class="sr-only">{{ __('Number of results per page', 'pressbooks-network-catalog') }}</label>
-	<select class="results-per-page" aria-labelledby="per-page-label">
-		<option>{{ sprintf(__('%d results', 'pressbooks-network-catalog'), 10) }}</option>
-		<option>{{ sprintf(__('%d results', 'pressbooks-network-catalog'), 20) }}</option>
-		<option>{{ sprintf(__('%d results', 'pressbooks-network-catalog'), 50) }}</option>
-	</select>
+        <label id="per-page-label" class="sr-only">{{ __('Number of results per page', 'pressbooks-network-catalog') }}</label>
+        <select class="results-per-page" aria-labelledby="per-page-label">
+            <option>{{ sprintf(__('%d results', 'pressbooks-network-catalog'), 10) }}</option>
+            <option>{{ sprintf(__('%d results', 'pressbooks-network-catalog'), 20) }}</option>
+            <option>{{ sprintf(__('%d results', 'pressbooks-network-catalog'), 50) }}</option>
+        </select>
 
-	<label id="sort-by-label" class="sr-only">{{ __('Sort results by', 'pressbooks-network-catalog') }}</label>
-	<select class="results-sort-by" aria-labelledby="sort-by-label">
-		<option>{{ __('Sort by relevance', 'pressbooks-network-catalog')}}</option>
-		<option>{{ __('Sort by recently updated', 'pressbooks-network-catalog') }}</option>
-		<option>{{ __('Sort by title (A-Z)', 'pressbooks-network-catalog') }}</option>
-	</select>
-</div>
+        <label id="sort-by-label" class="sr-only">{{ __('Sort results by', 'pressbooks-network-catalog') }}</label>
+        <select class="results-sort-by" aria-labelledby="sort-by-label">
+            <option>{{ __('Sort by relevance', 'pressbooks-network-catalog')}}</option>
+            <option>{{ __('Sort by recently updated', 'pressbooks-network-catalog') }}</option>
+            <option>{{ __('Sort by title (A-Z)', 'pressbooks-network-catalog') }}</option>
+        </select>
+    </div>
+</form>
 
 <div>
 	<h2 class="result-stats">{{ sprintf(__('%d Results for ‘%s’', 'pressbooks-network-catalog'), 2, 'Historical Agriculture') }}</h2>
--- resources/views/partials/sidebar-filters.blade.php
+++ resources/views/partials/sidebar-filters.blade.php
@@ -5,11 +5,11 @@
 			<span>&#8964;</span>
 		</button>
 	</span>
-	<ul x-cloak x-bind:class="!open && 'hidden'">
-		@foreach($subjects as $key => $subject)
-			<li>{{ $subject }}</li>
-		@endforeach
-	</ul>
+    <ul x-cloak x-bind:class="!open && 'hidden'">
+        @foreach($subjects as $key => $subject)
+            <li>{{ $subject }}</li>
+        @endforeach
+    </ul>
 </div>
 <div class="side-filter" x-data="{open: false}">
 	<span>
@@ -18,11 +18,11 @@
 			<span>&#8964;</span>
 		</button>
 	</span>
-	<ul x-cloak x-bind:class="!open && 'hidden'">
-		@foreach($licenses as $key => $license)
-			<li>{{ $license }}</li>
-		@endforeach
-	</ul>
+    <ul x-cloak x-bind:class="!open && 'hidden'">
+        @foreach($licenses as $key => $license)
+            <li>{{ $license }}</li>
+        @endforeach
+    </ul>
 </div>
 <div class="side-filter" x-data="{open: false}">
 	<span>
@@ -31,10 +31,10 @@
 			<span>&#8964;</span>
 		</button>
 	</span>
-	<div x-cloak x-bind:class="!open && 'hidden'">
-		<input type="date" name="from" />
-		<input type="date" name="to" />
-	</div>
+    <div x-cloak x-bind:class="!open && 'hidden'">
+        <input type="date" name="from"/>
+        <input type="date" name="to"/>
+    </div>
 </div>
 <div class="side-filter" x-data="{open: false}">
 	<span>
@@ -43,11 +43,11 @@
 			<span>&#8964;</span>
 		</button>
 	</span>
-	<ul x-cloak x-bind:class="!open && 'hidden'">
-		@foreach($institutions as $institution)
-			<li>{{ $institution }}</li>
-		@endforeach
-	</ul>
+    <ul x-cloak x-bind:class="!open && 'hidden'">
+        @foreach($institutions as $institution)
+            <li>{{ $institution }}</li>
+        @endforeach
+    </ul>
 </div>
 <div class="side-filter" x-data="{open: false}">
 	<span>
@@ -56,22 +56,25 @@
 			<span>&#8964;</span>
 		</button>
 	</span>
-	<ul x-cloak x-bind:class="!open && 'hidden'">
-		<li>No available filters at the moment</li>
-	</ul>
+    <ul x-cloak x-bind:class="!open && 'hidden'">
+        <li>No available filters at the moment</li>
+    </ul>
 </div>
-<div class="side-filter" x-data="{open: false}">
+<div class="side-filter" x-data="{open: $store.filters.h5p === true}">
 	<span>
 		<button x-on:click="open = !open" x-bind:aria-expanded="open">
 			<span>{{ __('H5P Activites', 'pressbooks-network-catalog') }}</span>
 			<span>&#8964;</span>
 		</button>
 	</span>
-	<div x-cloak x-bind:class="!open && 'hidden'">
-		<input type="number" name="min" placeholder="{{ __('At least N H5P activities', 'pressbooks-network-catalog') }}" />
-		<input type="number" name="max" placeholder="{{ __('At most N H5P actitivies', 'pressbooks-network-catalog') }}" />
-	</div>
+    <div x-cloak x-bind:class="!open && 'hidden'">
+        <label for="h5p">
+            {{ __('Has H5P activities', 'pressbooks-network-catalog') }}
+            <input x-data id="h5p" type="checkbox" name="h5p" :checked="$store.filters.h5p === true"
+                   x-model="$store.filters.h5p" @change="$store.filters.toggle('h5p')"/>
+        </label>
+    </div>
 </div>
 <button class="reset-filters">
-	{{ __('Clear filters', 'pressbooks-network-catalog') }}
+    {{ __('Clear filters', 'pressbooks-network-catalog') }}
 </button>
