b7c480b98c2fc8df8d49c44da4733f3ab40b240a
Ricardo Aragon <richard015ar@gmail.com>
2022-09-06 15:14:07 -0300

    feat: add sort_by param handling

--- src/BooksRequestManager.php
+++ src/BooksRequestManager.php
@@ -29,11 +29,84 @@ class BooksRequestManager
 	private int $defaultPerPage = 15;
 
 	/**
+	 * Get parameters allowed to be used in the request.
+	 *
+	 * @var Collection
+	 */
+	private Collection $allowedParams;
+
+	/**
 	 * @param Collection $bookFields
 	 */
 	public function __construct(Collection $bookFields)
 	{
 		$this->bookFields = $bookFields;
+		$this->allowedParams = Collection::make([
+			'pg' => [
+				'type' => 'string',
+				'default' => 1,
+				'filter' => FILTER_SANITIZE_STRING,
+			],
+			'per_page' => [
+				'type' => 'string',
+				'default' => $this->defaultPerPage,
+				'filter' => FILTER_SANITIZE_STRING,
+			],
+			'subjects' => [
+				'type' => 'array',
+				'field' => 'subjects',
+			],
+			'licenses' => [
+				'type' => 'array',
+				'field' => 'licenses',
+			],
+			'institutions' => [
+				'type' => 'array',
+				'field' => 'institutions',
+			],
+			'publishers' => [
+				'type' => 'array',
+				'field' => 'publishers',
+			],
+			'search' => [
+				'type' => 'string',
+				'filter' => FILTER_SANITIZE_STRING,
+			],
+			'h5p' => [
+				'type' => 'string',
+				'filter' => FILTER_SANITIZE_STRING,
+				'field' => 'h5p',
+			],
+			'updated_from' => [
+				'type' => 'string',
+				'regex' => '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
+				'filter' => FILTER_VALIDATE_REGEXP,
+				'sqlOperator' => '>=',
+				'field' => 'last_updated',
+			],
+			'updated_to' => [
+				'type' => 'string',
+				'regex' => '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
+				'filter' => FILTER_VALIDATE_REGEXP,
+				'sqlOperator' => '<=',
+				'field' => 'last_updated',
+			],
+			'sort_by' => [
+				'type' => 'string',
+				'filter' => FILTER_SANITIZE_STRING,
+				'default' => 'last_updated',
+				'allowedValues' => [
+					'last_updated' => [
+						'field' => 'updatedAt',
+						'order' => 'DESC',
+					],
+					'title' => [
+						'field' => 'title',
+						'order' => 'ASC',
+					],
+				],
+			],
+		]);
 		$this->request = Request::capture();
 	}
 
@@ -44,16 +117,22 @@ class BooksRequestManager
 	 */
 	public function validateRequest(): bool
 	{
-		$filterableColumns = $this->bookFields->where('filterable', true);
 		$valid = true;
-		$filterableColumns->each(function ($config) use (&$valid) {
-			$filter = $config['filterColumn'];
+		$this->allowedParams->each(function ($config, $filter) use (&$valid) {
+			// It could be improved with filter_var_array, see https://www.php.net/manual/es/function.filter-var-array.php
 			if (isset($this->request[$filter])) {
 				if (empty($this->request[$filter])) {
 					$this->request->request->remove($filter);
 				} elseif (
 					gettype($this->request[$filter]) !== $config['type'] ||
-					(isset($config['regex']) && ! preg_match($config['regex'], $this->request[$filter]))
+					(
+						isset($config['regex']) &&
+						filter_var($this->request[$filter], $config['filter'], ['options' => ['regexp' => $config['regex']]]) === false
+					) ||
+					(
+						! isset($config['regex']) && isset($config['filter']) &&
+						filter_var($this->request[$filter], $config['filter']) === false
+					)
 				) {
 					$valid = false;
 
@@ -61,16 +140,6 @@ class BooksRequestManager
 				}
 			}
 		});
-
-		foreach (['pg', 'per_page'] as $param) {
-			if (empty($this->request[$param])) {
-				$this->request->request->remove($param);
-				continue;
-			}
-			if (isset($this->request[$param]) && ! is_numeric($this->request[$param])) {
-				$valid = false;
-			}
-		}
 
 		return $valid;
 	}
@@ -107,43 +176,48 @@ class BooksRequestManager
 			return '';
 		}
 
-		$filtertableColumns = $this->bookFields->where('filterable', true);
+		$filtearableColumns = $this->bookFields->where('filterable', true);
 
 		$sqlQueryConditions = [];
 
 		global $wpdb;
 
-		$filtertableColumns->each(function ($config) use (&$sqlQueryConditions, $wpdb) {
-			$filter = $config['filterColumn'];
-			if (isset($this->request[$filter]) && ! empty($this->request[$filter])) {
-				if (isset($config['conditionQueryType'])) {
-					if ($config['conditionQueryType'] === 'standard') {
-						$in_placeholder = array_fill(0, count($this->request[$filter]), '%s');
-						$sqlQueryConditions[] = $config['alias'].
-							$wpdb->prepare(' IN ('.implode(', ', $in_placeholder).')', $this->request[$filter]);
-					}
-					if ($config['conditionQueryType'] === 'subquery') {
-						$in = '';
-						foreach ($this->request[$filter] as $filterValue) {
-							$in .= $wpdb->prepare('%s', $filterValue).',';
-						}
-						$in = rtrim($in, ',');
-						$column = $config['column'];
-						$sqlQueryConditions[] = " blog_id IN (SELECT blog_id FROM {$wpdb->blogmeta}
+		$this->allowedParams->each(function ($paramConfig, $filter) use (&$sqlQueryConditions, $wpdb, $filtearableColumns) {
+			if (isset($this->request[$filter]) && ! empty($this->request[$filter]) && isset($paramConfig['field'])) {
+				$config = $filtearableColumns->where('filterColumn', $paramConfig['field'])->first();
+				if ($config['conditionQueryType']) {
+					switch ($config['conditionQueryType']) {
+						case 'standard':
+							$in_placeholder = array_fill(0, count($this->request[$filter]), '%s');
+							$sqlQueryConditions[] = $config['alias'].
+								$wpdb->prepare(' IN ('.implode(', ', $in_placeholder).')', $this->request[$filter]);
+							break;
+						case 'subquery':
+							$in = '';
+							foreach ($this->request[$filter] as $filterValue) {
+								$in .= $wpdb->prepare('%s', $filterValue).',';
+							}
+							$in = rtrim($in, ',');
+							$column = $config['column'];
+							$sqlQueryConditions[] = " blog_id IN (SELECT blog_id FROM {$wpdb->blogmeta}
                             WHERE meta_key = '$column' AND meta_value IN ($in) GROUP BY blog_id)";
-					}
-					if ($config['conditionQueryType'] === 'date') {
-						$column = $config['alias'];
-						$sqlQueryConditions[] = "UNIX_TIMESTAMP($column) > UNIX_TIMESTAMP(".
-												$wpdb->prepare('%s', $this->request[$filter]).')';
-					}
-					if ($config['conditionQueryType'] === 'numeric') {
-						$sqlQueryConditions[] = $config['alias'].' > 0';
+							break;
+						case 'date':
+							if (isset($paramConfig['sqlOperator'])) {
+								$column = $config['alias'];
+								$sqlOperator = $paramConfig['sqlOperator'];
+								$sqlQueryConditions[] = "UNIX_TIMESTAMP($column) $sqlOperator UNIX_TIMESTAMP(".
+									$wpdb->prepare('%s', $this->request[$filter]).')';
+							}
+							break;
+						case 'numeric':
+							$sqlQueryConditions[] = $config['alias'].' > 0';
+							break;
 					}
 				}
 			}
 		});
-		if (isset($this->request['search']) && ! empty($this->request['search']) && is_string($this->request['search'])) {
+		if (isset($this->request['search']) && ! empty($this->request['search'])) {
 			$sqlQueryConditions[] = $this->getSqlSearchConditionsForCatalogQuery();
 		}
 
@@ -161,7 +235,25 @@ class BooksRequestManager
 		$searchableColumns = $this->bookFields->where('searchable', true);
 
 		return '('.$searchableColumns->map(function ($field) use ($wpdb) {
-			return $field['alias'].' LIKE '.$wpdb->prepare('%s', '%'.$this->request['search'].'%');
+			return 'LOWER('.$field['alias'].') LIKE '.$wpdb->prepare('%s', '%'.strtolower($this->request['search']).'%');
 		})->implode(' OR ').')';
+	}
+
+	/**
+	 * Get SQL Query Order By for books catalog query.
+	 *
+	 * @return string
+	 */
+	public function getSqlOrderByForCatalogQuery(): string
+	{
+		if ($this->request['sort_by'] &&
+		   in_array($this->request['sort_by'], array_keys($this->allowedParams->get('sort_by')['allowedValues']))
+		) {
+			$orderBy = $this->allowedParams->get('sort_by')['allowedValues'][$this->request['sort_by']];
+
+			return ' ORDER BY '.$orderBy['field'].' '.$orderBy['order'];
+		}
+
+		return '';
 	}
 }
