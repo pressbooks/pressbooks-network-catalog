3de62e1ec03f6d64667c59a1234c6d687aad5557
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-09-06 15:15:53 -0400

    feat: improve search box styling

--- resources/views/partials/search-box.blade.php
+++ resources/views/partials/search-box.blade.php
@@ -1,14 +1,16 @@
 <div class="search-box">
     <div class="search">
         <label id="search-input-label" class="sr-only">{{ __('Search', 'pressbooks-network-catalog') }}</label>
-        <input
-			type="search"
-			name="search"
-			placeholder="{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}"
-			value="{{ $request->search }}"
-			aria-labelledby="search-input-label"
-		/>
-        <button id="search" type="submit">
+		<div class="search-input">
+			<input
+				type="search"
+				name="search"
+				placeholder="{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}"
+				value="{{ $request->search }}"
+				aria-labelledby="search-input-label"
+			/>
+		</div>
+		<button id="search" type="submit">
 			<span class="sr-only">{{ __('Search', 'pressbooks-network-catalog') }}</span>
 			@include('PressbooksNetworkCatalog::icons.search')
 		</button>
