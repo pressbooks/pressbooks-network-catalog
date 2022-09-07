ce5f90a47f11a61106d28bbdb03c78fa379d1983
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-09-07 12:03:52 -0400

    fix: check dependencies before loading

--- pressbooks-network-catalog.php
+++ pressbooks-network-catalog.php
@@ -15,6 +15,18 @@ use PressbooksNetworkCatalog\PressbooksNetworkCatalog;
 
 const IS_VITE_DEVELOPMENT = false;
 
-require __DIR__ . '/vendor/autoload.php';
+if ( ! class_exists( 'Pressbooks\Book' ) ) {
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	} else {
+		$title = __( 'Missing dependencies', 'pressbooks-network-catalog' );
+		$body = __(
+			'Please run <code>composer install</code> from the root of the Pressbooks Network Catalog plugin directory.',
+			'pressbooks-network-catalog'
+		);
+
+		wp_die( "<h1>{$title}</h1><p>{$body}</p>" );
+	}
+}
 
 add_action( 'plugins_loaded', [PressbooksNetworkCatalog::class, 'init']);
