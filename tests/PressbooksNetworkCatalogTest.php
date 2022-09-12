1f5676e1de043b349bbdc35486fe68f666ee5c78
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-09-12 16:49:38 -0400

    feat: add example test

---
+++ tests/PressbooksNetworkCatalogTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+class PressbooksNetworkCatalogTest extends \WP_UnitTestCase
+{
+    use \utilsTrait;
+
+    /**
+     * @test
+     * @group network-catalog
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
