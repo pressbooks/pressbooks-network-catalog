ce731ea69a65535fdf16bb7c55993ae6ff31c9ec
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-09-13 17:31:26 -0400

    chore: update pressbooks network catalog test

--- tests/PressbooksNetworkCatalogTest.php
+++ tests/PressbooksNetworkCatalogTest.php
@@ -3,41 +3,24 @@
 namespace Tests;
 
 use PressbooksNetworkCatalog\PressbooksNetworkCatalog;
+use WP_UnitTestCase;
 
-class PressbooksNetworkCatalogTest extends \WP_UnitTestCase
+class PressbooksNetworkCatalogTest extends WP_UnitTestCase
 {
-	use \utilsTrait;
-
-	public function setUp(): void
-	{
-		add_action('admin_init', '\Aldine\Actions\hide_catalog_content_editor');
-	}
-
 	/**
 	 * @test
 	 * @group network-catalog
 	 */
 	public function it_adds_the_network_catalog_filter(): void
 	{
-		$this->assertNull(
-			$this->filter('pb_network_catalog')
+		$this->assertFalse(
+			has_action('pb_network_catalog')
 		);
 
 		PressbooksNetworkCatalog::init();
 
-		$this->assertInstanceOf(
-			\WP_Hook::class, $this->filter('pb_network_catalog')
+		$this->assertTrue(
+			has_action('pb_network_catalog')
 		);
-	}
-
-	/**
-	 * @param string $name
-	 * @return \WP_Hook|null
-	 */
-	protected function filter(string $name): ?\WP_Hook
-	{
-		global $wp_filter;
-
-		return $wp_filter[$name] ?? null;
 	}
 }
