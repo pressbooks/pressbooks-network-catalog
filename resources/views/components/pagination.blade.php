4fc42d4cc5023449f143acbefebfa0e497b8aad9
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-09-27 15:10:58 -0400

    fix: display link only if needed

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -3,41 +3,45 @@
 		class="pagination"
 		role="navigation"
 		aria-label="{{ sprintf(__('%s pagination', 'pressbooks-network-catalog'), $placement) }}"
-        x-data x-id="['pagination-dropdown']"
+		x-data x-id="['pagination-dropdown']"
 	>
-		<a
-			class="pagination-item"
-			rel="prev"
+		@if($pagination['currentPage'] > 1)
+			<a
+				class="pagination-item"
+				rel="prev"
 
-			href="{{ $request->fullUrlWithQuery(['pg' => $pagination['previousPage']]) }}"
-			disabled
-		>
-			<span class="sr-only">{{ __('Go to previous page', 'pressbooks-network-catalog') }}</span>
-			@include('PressbooksNetworkCatalog::icons.chevron-left')
-		</a>
+				href="{{ $request->fullUrlWithQuery(['pg' => $pagination['previousPage']]) }}"
+				disabled
+			>
+				<span class="sr-only">{{ __('Go to previous page', 'pressbooks-network-catalog') }}</span>
+				@include('PressbooksNetworkCatalog::icons.chevron-left')
+			</a>
+		@endif
 
 		<div>
 			<label class="sr-only" :for="$id('pagination-dropdown')">{{ sprintf(__('Go to page # of %d', 'pressbooks-network-catalog'), $pagination['totalPages']) }}</label>
 			<select name="pg" :id="$id('pagination-dropdown')">
 				@foreach(range(1, $pagination['totalPages']) as $page)
 					<option
-                        {{ $page === $pagination['currentPage'] ? 'selected' : '' }}
-                        value="{{ $page }}"
-                    >
-                        {{ $page }}
-                    </option>
+						{{ $page === $pagination['currentPage'] ? 'selected' : '' }}
+						value="{{ $page }}"
+					>
+						{{ $page }}
+					</option>
 				@endforeach
 			</select>
 			{{ sprintf(__('of %d', 'pressbooks-network-catalog'), $pagination['totalPages']) }}
 		</div>
 
-		<a
-			class="pagination-item"
-			rel="next"
-			href="{{ $request->fullUrlWithQuery(['pg' => $pagination['nextPage']]) }}"
-		>
-			<span class="sr-only">{{ __('Go to next page', 'pressbooks-network-catalog') }}</span>
-			@include('PressbooksNetworkCatalog::icons.chevron-right')
-		</a>
+		@if($pagination['currentPage'] < $pagination['totalPages'])
+			<a
+				class="pagination-item"
+				rel="next"
+				href="{{ $request->fullUrlWithQuery(['pg' => $pagination['nextPage']]) }}"
+			>
+				<span class="sr-only">{{ __('Go to next page', 'pressbooks-network-catalog') }}</span>
+				@include('PressbooksNetworkCatalog::icons.chevron-right')
+			</a>
+		@endif
 	</nav>
 @endunless
