f2fcfd5ba4d0da40067fb2a0c3eeb86bce5d5793
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-09-29 17:05:07 -0400

    fix: improve book message when results are empty

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,42 +1,46 @@
 @php(get_header())
 
 <main>
-    <div class="hero" style="background-image: url({{$catalogBg}})">
-        <div>
-            <div class="hero-content">
-                <h1 class="catalog-header">{!! wp_kses_post(get_the_title()) !!}</h1>
-                <div class="catalog-content">
-                    {!! the_content() !!}
-                </div>
-            </div>
-        </div>
-    </div>
-    <form method="get" action="{{ $request->url() }}" id="network-catalog-form">
-        <div class="mobile-bar search-box">
-            @include('PressbooksNetworkCatalog::components.search-input')
-        </div>
-        <div class="network-catalog">
-            <aside class="side-filters">
-                @include('PressbooksNetworkCatalog::partials.sidebar-filters')
-            </aside>
+	<div class="hero" style="background-image: url({{$catalogBg}})">
+		<div>
+			<div class="hero-content">
+				<h1 class="catalog-header">{!! wp_kses_post(get_the_title()) !!}</h1>
+				<div class="catalog-content">
+					{!! the_content() !!}
+				</div>
+			</div>
+		</div>
+	</div>
+	<form method="get" action="{{ $request->url() }}" id="network-catalog-form">
+		<div class="mobile-bar search-box">
+			@include('PressbooksNetworkCatalog::components.search-input')
+		</div>
+		<div class="network-catalog">
+			<aside class="side-filters">
+				@include('PressbooksNetworkCatalog::partials.sidebar-filters')
+			</aside>
 
-            <div data-barba="container" data-barba-namespace="home">
-                @include('PressbooksNetworkCatalog::partials.search-box')
+			<div data-barba="container" data-barba-namespace="home">
+				@include('PressbooksNetworkCatalog::partials.search-box')
 
-                <div class="book-cards">
+				<div class="book-cards">
 					@include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'top'])
 
-                    @forelse( $books as $book )
-                        @include('PressbooksNetworkCatalog::partials.book-card')
-                    @empty
-                        <p>No books have been added to the catalog yet.</p>
-                    @endforelse
+					@forelse( $books as $book )
+						@include('PressbooksNetworkCatalog::partials.book-card')
+					@empty
+						@if($catalogHasBooks)
+							<p>{{ __('Sorry, no results were found. You may want to check your spelling, use alternative terms with similar meanings, or clear one or more filters.', 'pressbooks-network-catalog') }}</p>
+						@else
+							<p>{{ __('No books have been added to the catalog yet.', 'pressbooks-network-catalog') }}</p>
+						@endif
+					@endforelse
 
 					@include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'bottom'])
-                </div>
-            </div>
-        </div>
-    </form>
+				</div>
+			</div>
+		</div>
+	</form>
 </main>
 
 @php(get_footer())
