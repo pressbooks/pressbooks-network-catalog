787a06751fed4ca2f9c4a86603203ca31c46d303
arzola <oscararzola@gmail.com>
2022-09-28 21:27:43 -0400

    fix: mobile order

---
+++ resources/views/components/search-input.blade.php
@@ -0,0 +1,16 @@
+<div class="search">
+    <label id="search-input-label" class="sr-only">{{ __('Search', 'pressbooks-network-catalog') }}</label>
+    <div class="search-input">
+        <input
+            type="search"
+            name="search"
+            placeholder="{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}"
+            value="{{ $request->search }}"
+            aria-labelledby="search-input-label"
+        />
+    </div>
+    <button id="search" type="submit">
+        <span class="sr-only">{{ __('Search', 'pressbooks-network-catalog') }}</span>
+        @include('PressbooksNetworkCatalog::icons.search')
+    </button>
+</div>
