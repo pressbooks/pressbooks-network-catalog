526aa94a0ff659356fc8e5840eecb8f355d2c575
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-10-13 14:29:43 -0400

    fix: improve sectioning (#88)
    * fix: improve sectioning to better landmark for screen readers
    
    * fix: indentation

--- resources/views/partials/search-box.blade.php
+++ resources/views/partials/search-box.blade.php
@@ -1,43 +1,43 @@
 <div class="search-box">
-    @include('PressbooksNetworkCatalog::components.search-input')
+	@include('PressbooksNetworkCatalog::components.search-input')
 
 	<div class="refine-filters">
-        @include('PressbooksNetworkCatalog::components.dropdown', [
-    	'label' => __('Results per page', 'pressbooks-network-catalog'),
-    	'name' => 'per_page',
-    	'default' => 10,
-    	'options' => [
-    		10 => sprintf(__('%d results', 'pressbooks-network-catalog'), 10),
-    		20 => sprintf(__('%d results', 'pressbooks-network-catalog'), 20),
-    		50 => sprintf(__('%d results', 'pressbooks-network-catalog'), 50),
-		]
-	])
+		@include('PressbooksNetworkCatalog::components.dropdown', [
+			'label' => __('Results per page', 'pressbooks-network-catalog'),
+			'name' => 'per_page',
+			'default' => 10,
+			'options' => [
+				10 => sprintf(__('%d results', 'pressbooks-network-catalog'), 10),
+				20 => sprintf(__('%d results', 'pressbooks-network-catalog'), 20),
+				50 => sprintf(__('%d results', 'pressbooks-network-catalog'), 50),
+			]
+		])
 
-        @include('PressbooksNetworkCatalog::components.dropdown', [
-            'label' => __('Sort by', 'pressbooks-network-catalog'),
-            'name' => 'sort_by',
-            'default' => 'last_updated',
-            'options' => [
-                //'relevance' => __('Sort by relevance', 'pressbooks-network-catalog'),
-                'last_updated' => __('Sort by recently updated', 'pressbooks-network-catalog'),
-                'title' => __('Sort by title (A-Z)', 'pressbooks-network-catalog'),
-            ]
-        ])
-    </div>
+		@include('PressbooksNetworkCatalog::components.dropdown', [
+			'label' => __('Sort by', 'pressbooks-network-catalog'),
+			'name' => 'sort_by',
+			'default' => 'last_updated',
+			'options' => [
+				//'relevance' => __('Sort by relevance', 'pressbooks-network-catalog'),
+				'last_updated' => __('Sort by recently updated', 'pressbooks-network-catalog'),
+				'title' => __('Sort by title (A-Z)', 'pressbooks-network-catalog'),
+			]
+		])
+	</div>
 </div>
 <div>
-    @if(!empty($request->search) && $request->has('search'))
-        <span class="result-stats">
-            {{ sprintf(_n('%d result for ‘%s’', '%d results for ‘%s’', $pagination['total'], 'pressbooks-network-catalog'), $pagination['total'], $request->search ) }}
-        </span>
-    @elseif($pagination['currentPage'] <= $pagination['totalPages'])
-        <span class="result-stats">
-            {{ sprintf(_n('%d result', '%d results', $pagination['total'], 'pressbooks-network-catalog'), $pagination['total']) }}
-        </span>
-    @endif
+	@if(!empty($request->search) && $request->has('search'))
+		<span class="result-stats">
+			{{ sprintf(_n('%d result for ‘%s’', '%d results for ‘%s’', $pagination['total'], 'pressbooks-network-catalog'), $pagination['total'], $request->search ) }}
+		</span>
+	@elseif($pagination['currentPage'] <= $pagination['totalPages'])
+		<span class="result-stats">
+			{{ sprintf(_n('%d result', '%d results', $pagination['total'], 'pressbooks-network-catalog'), $pagination['total']) }}
+		</span>
+	@endif
 
 	@if($request->activeFilters->isNotEmpty())
-		<div class="applied-filters" x-data>
+		<section class="applied-filters" x-data aria-label="{{ __('Applied filters', 'pressbooks-network-catalog') }}">
 			@foreach($request->activeFilters as $filter)
 				<div class="applied-filter">
 					<span>{{ $filter['label'] }}</span>
@@ -47,6 +47,6 @@
 					</button>
 				</div>
 			@endforeach
-		</div>
+		</section>
 	@endif
 </div>
