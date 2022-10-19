f65a57f1082277c3ea6f529f9d1190d429012f07
Oscar Arzola <oscararzola@gmail.com>
2022-10-19 12:23:31 -0400

    fix: add aria label (#101)

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,5 +1,5 @@
-<select x-data @change="changeOnSelect" name="{{ $name }}">
-    @foreach($options as $value => $label)
-        <option value="{{ $value }}" {{ $value == $default ? 'selected' : '' }}>{{ $label }}</option>
+<select x-data @change="changeOnSelect" name="{{ $name }}" aria-label="{{ $label }}">
+    @foreach($options as $value => $text)
+        <option value="{{ $value }}" {{ $value == $default ? 'selected' : '' }}>{{ $text }}</option>
     @endforeach
 </select>
