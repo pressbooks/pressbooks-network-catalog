7d27617678a080beb9359f8fb5d5566512a9e732
Ricardo Aragon <richard015ar@gmail.com>
2022-10-20 10:25:26 -0300

    fix: identation issue

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -11,40 +11,40 @@
 			</div>
 		</div>
 	</div>
-    <div id="catalog">
-        <form method="get" action="{{ $request->url() }}#catalog" id="network-catalog-form">
-            <section class="mobile-bar search-box" aria-label="{{ __('Search', 'pressbooks-network-catalog') }}">
-                @include('PressbooksNetworkCatalog::components.search-input')
-            </section>
-            <div class="network-catalog">
-                <section class="side-filters" aria-label="{{ __('Filters', 'pressbooks-network-catalog') }}">
-                    @include('PressbooksNetworkCatalog::partials.sidebar-filters')
-                </section>
+	<div id="catalog">
+		<form method="get" action="{{ $request->url() }}#catalog" id="network-catalog-form">
+			<section class="mobile-bar search-box" aria-label="{{ __('Search', 'pressbooks-network-catalog') }}">
+				@include('PressbooksNetworkCatalog::components.search-input')
+			</section>
+			<div class="network-catalog">
+				<section class="side-filters" aria-label="{{ __('Filters', 'pressbooks-network-catalog') }}">
+					@include('PressbooksNetworkCatalog::partials.sidebar-filters')
+				</section>
 
-                <div>
-                    <section aria-label="{{ __('Search, pagination, and sorting', 'pressbooks-network-catalog') }}">
-                        @include('PressbooksNetworkCatalog::partials.search-box')
-                    </section>
+				<div>
+					<section aria-label="{{ __('Search, pagination, and sorting', 'pressbooks-network-catalog') }}">
+						@include('PressbooksNetworkCatalog::partials.search-box')
+					</section>
 
-                    @include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'top'])
+					@include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'top'])
 
-                    <section class="book-cards" aria-label="{{ __('Book list', 'pressbooks-network-catalog') }}">
-                        @forelse( $books as $book )
-                            @include('PressbooksNetworkCatalog::partials.book-card')
-                        @empty
-                            @if($catalogHasBooks)
-                                <p>{{ __('Sorry, no results were found. You may want to check your spelling, use alternative terms with similar meanings, or clear one or more filters.', 'pressbooks-network-catalog') }}</p>
-                            @else
-                                <p>{{ __('No books have been added to the catalog yet.', 'pressbooks-network-catalog') }}</p>
-                            @endif
-                        @endforelse
-                    </section>
+					<section class="book-cards" aria-label="{{ __('Book list', 'pressbooks-network-catalog') }}">
+						@forelse( $books as $book )
+							@include('PressbooksNetworkCatalog::partials.book-card')
+						@empty
+							@if($catalogHasBooks)
+								<p>{{ __('Sorry, no results were found. You may want to check your spelling, use alternative terms with similar meanings, or clear one or more filters.', 'pressbooks-network-catalog') }}</p>
+							@else
+								<p>{{ __('No books have been added to the catalog yet.', 'pressbooks-network-catalog') }}</p>
+							@endif
+						@endforelse
+					</section>
 
-                    @include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'bottom'])
-                </div>
-            </div>
-        </form>
-    </div>
+					@include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'bottom'])
+				</div>
+			</div>
+		</form>
+	</div>
 </main>
 
 @php(get_footer())
