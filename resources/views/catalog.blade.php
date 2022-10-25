096c922e8e4bc264fe7fcbe54176f877ddac8416
arzola <oscararzola@gmail.com>
2022-10-25 13:41:21 -0400

    fix: attempt to update mariadb installation

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -11,41 +11,43 @@
 			</div>
 		</div>
 	</div>
-	<form method="get" action="{{ $request->url() }}#catalog" id="network-catalog-form">
-		<section class="mobile-bar search-box" aria-label="{{ __('Search', 'pressbooks-network-catalog') }}">
-			@include('PressbooksNetworkCatalog::components.search-input')
-		</section>
-		<div class="network-catalog">
-            <div x-data="{open: false}">
-                @include('PressbooksNetworkCatalog::partials.refine-filters', ['class' => 'order-mobile'])
-                <section :class="open ? 'open': 'hidden'" class="side-filters" aria-label="{{ __('Filters', 'pressbooks-network-catalog') }}" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" x-cloak :class="!open && 'hidden'">
-                    @include('PressbooksNetworkCatalog::partials.sidebar-filters')
-                </section>
+	<div id="catalog">
+        <form method="get" action="{{ $request->url() }}#catalog" id="network-catalog-form">
+            <section class="mobile-bar search-box" aria-label="{{ __('Search', 'pressbooks-network-catalog') }}">
+                @include('PressbooksNetworkCatalog::components.search-input')
+            </section>
+            <div class="network-catalog">
+                <div x-data="{open: false}">
+                    @include('PressbooksNetworkCatalog::partials.refine-filters', ['class' => 'order-mobile'])
+                    <section :class="open ? 'open': 'hidden'" class="side-filters" aria-label="{{ __('Filters', 'pressbooks-network-catalog') }}" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" x-cloak :class="!open && 'hidden'">
+                        @include('PressbooksNetworkCatalog::partials.sidebar-filters')
+                    </section>
+                </div>
+
+                <div class="results">
+                    <section aria-label="{{ __('Search, pagination, and sorting', 'pressbooks-network-catalog') }}">
+                        @include('PressbooksNetworkCatalog::partials.search-box')
+                    </section>
+
+                    @include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'top'])
+
+                    <section class="book-cards" aria-label="{{ __('Book list', 'pressbooks-network-catalog') }}">
+                        @forelse( $books as $book )
+                            @include('PressbooksNetworkCatalog::partials.book-card')
+                        @empty
+                            @if($catalogHasBooks)
+                                <p>{{ __('Sorry, no results were found. You may want to check your spelling, use alternative terms with similar meanings, or clear one or more filters.', 'pressbooks-network-catalog') }}</p>
+                            @else
+                                <p>{{ __('No books have been added to the catalog yet.', 'pressbooks-network-catalog') }}</p>
+                            @endif
+                        @endforelse
+                    </section>
+
+                    @include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'bottom'])
+                </div>
             </div>
-
-			<div class="results">
-				<section aria-label="{{ __('Search, pagination, and sorting', 'pressbooks-network-catalog') }}">
-					@include('PressbooksNetworkCatalog::partials.search-box')
-				</section>
-
-				@include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'top'])
-
-				<section class="book-cards" aria-label="{{ __('Book list', 'pressbooks-network-catalog') }}">
-					@forelse( $books as $book )
-						@include('PressbooksNetworkCatalog::partials.book-card')
-					@empty
-						@if($catalogHasBooks)
-							<p>{{ __('Sorry, no results were found. You may want to check your spelling, use alternative terms with similar meanings, or clear one or more filters.', 'pressbooks-network-catalog') }}</p>
-						@else
-							<p>{{ __('No books have been added to the catalog yet.', 'pressbooks-network-catalog') }}</p>
-						@endif
-					@endforelse
-				</section>
-
-				@include('PressbooksNetworkCatalog::components.pagination', ['placement' => 'bottom'])
-			</div>
-		</div>
-	</form>
+        </form>
+    </div>
 </main>
 
 @php(get_footer())
