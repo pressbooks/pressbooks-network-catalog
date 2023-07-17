e180953180ae2e4c68424460f07eec55470f6bc1
Steel Wagstaff <steel@pressbooks.com>
2023-07-17 13:00:02 -0700

    fix: a11y issues on catalog page (#247)
    * fix: a11y fixes for catalog
    
    * fix: a11y fixes for catalog

---
+++ resources/views/components/search-input-mobile.blade.php
@@ -0,0 +1,16 @@
+<div class="search">
+	<label id="search-input-mobile-label" class="sr-only">{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}</label>
+	<div class="search-input mobile">
+		<input
+			type="search"
+			name="search_term"
+			placeholder="{{ __('Search by title, author, keyword', 'pressbooks-network-catalog') }}"
+			value="{{ $request->search_term }}"
+			aria-labelledby="search-input-label"
+		/>
+	</div>
+	<button id="search-mobile" type="submit">
+		<span class="sr-only">{{ __('Search', 'pressbooks-network-catalog') }}</span>
+		@include('PressbooksNetworkCatalog::icons.search')
+	</button>
+</div>
