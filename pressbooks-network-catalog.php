e881b79577afc4c0b58b71f737685f5c6c1d74c0
SteelWagstaff <steel@pressbooks.com>
2024-02-22 17:09:31 -0800

    chore: add update mo action, update deps & version

--- pressbooks-network-catalog.php
+++ pressbooks-network-catalog.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pressbooks Network Catalog
  * Plugin URI: https://pressbooks.org
  * Description: Add a searchable, filterable catalog to the Pressbooks Aldine theme
- * Version: 1.3.4
+ * Version: 1.3.5
  * Author: Pressbooks (Book Oven Inc.)
  * Author URI: https://pressbooks.org
  * Text Domain: pressbooks-network-catalog
