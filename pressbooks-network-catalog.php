509c3396f9b385a52b89bcb6268fbd85866ae6c4
Ned Zimmerman <ned@bight.dev>
2024-04-08 14:42:29 -0300

    feat: use WordPress plugin dependency feature (#358)
    * feat: use WordPress plugin dependency feature
    
    * test: fix tests
    
    * fix: format files
    
    * chore(deps-dev): support env
    
    * test: remove dependency
    
    * test: fix test bootstrap
    
    * chore: update CI pipeline WP version
    
    ---------
    
    Co-authored-by: Felipe Dalcin <felipe@pressbooks.com>

--- pressbooks-network-catalog.php
+++ pressbooks-network-catalog.php
@@ -2,6 +2,8 @@
 /*
  * Plugin Name: Pressbooks Network Catalog
  * Plugin URI: https://pressbooks.org
+ * Requires at least: 6.5
+ * Requires Plugins: pressbooks
  * Description: Add a searchable, filterable catalog to the Pressbooks Aldine theme
  * Version: 1.3.5
  * Author: Pressbooks (Book Oven Inc.)
@@ -15,19 +17,5 @@
 use PressbooksNetworkCatalog\PressbooksNetworkCatalog;
 
 const IS_VITE_DEVELOPMENT = false;
-
-if ( ! class_exists( 'Pressbooks\Book' ) ) {
-	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-		require_once __DIR__ . '/vendor/autoload.php';
-	} else {
-		$title = __( 'Missing dependencies', 'pressbooks-network-catalog' );
-		$body = __(
-			'Please run <code>composer install</code> from the root of the Pressbooks Network Catalog plugin directory.',
-			'pressbooks-network-catalog'
-		);
-
-		wp_die( "<h1>{$title}</h1><p>{$body}</p>" );
-	}
-}
 
 add_action( 'plugins_loaded', [PressbooksNetworkCatalog::class, 'init']);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,16 @@ use Illuminate\Support\Collection;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-$tests_dir = env('WP_TESTS_DIR', '/tmp/wordpress-tests-lib');
+$tests_dir = getenv('WP_TESTS_DIR');
+if (! $tests_dir) {
+	$tests_dir = '/tmp/wordpress-tests-lib';
+}
 
 require_once "{$tests_dir}/includes/functions.php";
 
 tests_add_filter('muplugins_loaded', function () {
-	require_once dirname(__DIR__).'/wp-content/plugins/pressbooks/pressbooks.php';
-	require_once dirname(__DIR__).'/wp-content/plugins/pressbooks/tests/utils-trait.php';
+	require_once __DIR__.'/../../pressbooks/pressbooks.php';
+	require_once __DIR__.'/../../pressbooks/tests/utils-trait.php';
 });
 
 require_once "{$tests_dir}/includes/bootstrap.php";
