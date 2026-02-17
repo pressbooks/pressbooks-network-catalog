73743af0a4db97adbfbb7de3d08fa7e1695af7b0
Ricardo Aragon <richard015ar@gmail.com>
2026-02-17 18:01:23 +0100

    fix: add tests

--- tests/PressbooksNetworkCatalogTest.php
+++ tests/PressbooksNetworkCatalogTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests;
 
+use Pressbooks\DataCollector\Book;
 use PressbooksNetworkCatalog\PressbooksNetworkCatalog;
+use function Pressbooks\Metadata\get_in_catalog_option;
+use utilsTrait;
 
 class PressbooksNetworkCatalogTest extends TestCase
 {
+	use utilsTrait;
+
 	public function setUp(): void
 	{
 		parent::setUp();
@@ -42,5 +47,33 @@ class PressbooksNetworkCatalogTest extends TestCase
 
 		$this->assertNotEmpty($content);
 		$this->assertStringContainsString('<div class="network-catalog">', $content);
+	}
+
+	/**
+	 * @test
+	 * @group network-catalog
+	 */
+	public function it_removes_book_from_catalog_on_blog_deactivation(): void
+	{
+		$this->_book();
+
+		$blogId = get_current_blog_id();
+
+		update_option(get_in_catalog_option(), 1);
+		
+		restore_current_blog();
+
+		update_site_meta($blogId, Book::IN_CATALOG, 1);
+
+		PressbooksNetworkCatalog::init();
+
+		do_action('deactivate_blog', $blogId);
+
+		switch_to_blog($blogId);
+		$optionValue = get_option(get_in_catalog_option());
+		restore_current_blog();
+
+		$this->assertEquals(0, $optionValue);
+		$this->assertEquals(0, get_site_meta($blogId, Book::IN_CATALOG, true));
 	}
 }
