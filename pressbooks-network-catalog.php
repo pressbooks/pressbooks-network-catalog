89afa4da47ef085dfc63dad94f83f1a13d068cb9
pressbooks-ops <66340483+pressbooks-ops@users.noreply.github.com>
2026-07-21 13:54:07 -0400

    chore(dev): release 1.6.3

--- pressbooks-network-catalog.php
+++ pressbooks-network-catalog.php
@@ -6,7 +6,7 @@
  * Requires Plugins: pressbooks
  * Description: Add a searchable, filterable catalog to the Pressbooks Aldine theme
  * x-release-please-start-version
- * Version: 1.6.2
+ * Version: 1.6.3
  * x-release-please-end
  * Author: Pressbooks (Book Oven Inc.)
  * Author URI: https://pressbooks.org
